cdf9afa8c8eaf035224ea5a48fd6765a69015da9
usama khan <hafizahmed420@gmail.com>
2020-10-28 22:43:15 +0500

    1: SplashActivity 2: SignIn_signUp 3: SignUpActivity

---
+++ app/src/main/java/com/example/loadease/SignIn_signUp.java
@@ -0,0 +1,14 @@
+package com.example.loadease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn_signUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_sign_up);
+    }
+}
