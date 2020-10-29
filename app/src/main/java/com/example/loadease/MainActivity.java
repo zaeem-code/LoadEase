ba0d1261646418cb4734539f6d72e6435292af8a
usama khan <hafizahmed420@gmail.com>
2020-10-29 19:36:01 +0500

    1: SplashActivity 2: SignIn_signUp 3: SignUpActivity

--- app/src/main/java/com/example/loadease/MainActivity.java
+++ app/src/main/java/com/example/loadease/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.loadease;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
+
+
+//        asfddsafdas
     }
 }
--- app/src/main/java/com/example/loadease/SignIn_signUp.java
+++ app/src/main/java/com/example/loadease/SignIn_signUp.java
@@ -2,13 +2,49 @@ package com.example.loadease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class SignIn_signUp extends AppCompatActivity {
+public class SignIn_signUp extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout sign_in;
+    TextView sign_up;
+    ImageView facebook;
+    ImageView twitter;
+    ImageView google;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_sign_up);
+
+        sign_in = findViewById(R.id.sign_in);
+        sign_in.setOnClickListener(this);
+        sign_up = findViewById(R.id.sign_up);
+        sign_up.setOnClickListener(this);
+        facebook = findViewById(R.id.facebook_img);
+        facebook.setOnClickListener(this);
+        twitter = findViewById(R.id.twitter_img);
+        twitter.setOnClickListener(this);
+        google = findViewById(R.id.google_img);
+        google.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.sign_in){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }else if (id == R.id.sign_up){
+            Intent signUp = new Intent(this,SignUpActivity.class);
+            startActivity(signUp);
+        }
     }
 }
