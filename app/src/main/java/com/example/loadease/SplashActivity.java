11b14f6f1cf2d4accb85d8321825a9e644e6e41b
usama khan <hafizahmed420@gmail.com>
2020-11-03 18:55:59 +0500

    1: ProfileActivity 2: WalletAct

--- app/src/main/java/com/example/loadease/SplashActivity.java
+++ app/src/main/java/com/example/loadease/SplashActivity.java
@@ -25,6 +25,7 @@ public class SplashActivity extends AppCompatActivity {
                     finish();
                 } catch (Exception e) {
                 }
+//                etef
             }
         };
         // start thread
