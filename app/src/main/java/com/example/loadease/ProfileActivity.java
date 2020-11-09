16dc9a9fcfb964449dcbd087eccb2ee7c2699a5f
ChTpx <ChTpx@github.com>
2020-11-09 20:11:10 +0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/google-services.json
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/loadease/SignInActivity.java
    #	app/src/main/java/com/example/loadease/SignIn_signUp.java
    #	app/src/main/java/com/example/loadease/SignUpActivity.java
    #	app/src/main/java/com/example/loadeasex/OtpAct/OtpActivit.java
    #	app/src/main/java/com/example/loadeasex/SettingActivity.java
    #	app/src/main/java/com/example/loadeasex/SignInActivity.java
    #	app/src/main/java/com/example/loadeasex/SignIn_signUp.java
    #	app/src/main/java/com/example/loadeasex/SignIn_signUp/SignInActivity.java
    #	app/src/main/java/com/example/loadeasex/SignIn_signUp/SignIn_signUp.java
    #	app/src/main/java/com/example/loadeasex/SignIn_signUp/SignUpActivity.java
    #	app/src/main/java/com/example/loadeasex/SignUpActivity.java
    #	app/src/main/java/com/example/loadeasex/SplashActivity.java

--- app/src/main/java/com/example/loadease/ProfileActivity.java
+++ app/src/main/java/com/example/loadease/ProfileActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.loadeasex.R;
+
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
